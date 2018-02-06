b4c748c257c7f8b1064d21dd314f28d3e8298d05
Dzmitry Safarau <Dzmitry_Safarau@epam.com>
2018-02-06 18:16:54 +0300

    Added: logs generator.

---
+++ LogStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Http.BatchFormatters;
+
+namespace LogStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Http("http://localhost:31311", batchFormatter: new ArrayBatchFormatter())
+                .WriteTo.Console()
+                .CreateLogger();
+            var random = new Random();
+            while (true)
+            {
+                var level = (LogEventLevel)random.Next(0, 6);
+                var value = random.Next();
+                log.Write(level, "Executing some code with value {@value}", value);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
